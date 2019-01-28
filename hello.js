fdc8d6e7cb0df4e98a74a8d1e79bda92458104fc
Jesus Alvarez <jalvarez1@una.edu>
2019-01-28 16:38:56 +0000

    daily 1/28/19

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
-console.log("hello, we are here");
+const http = require('http');
+
+http.createServer(function(request,response){
+    //console.log(request);
+    response.writeHead(200);
+    response.write("<html><body><p>Hello, dog is running.</p><body></html>");
+    setTimeout(function() {
+        response.write("<p>Dog is really tired</p></body></html>")
+        response.end();
+    }, 5000);
+}).listen(8080);
+
+console.log('Listening on port 8080...');
